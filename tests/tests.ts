0800ec37fe947d752d5005631d4271b6866d17ca
dreamquality <dreamquality@users.noreply.github.com>
2025-11-02 14:45:13 +0000

    deploy: 1184ea827e2aa3387bbe643bd393f4bcb26d55d5

--- tests/tests.ts
+++
@@ -1,8 +0,0 @@
-import { test as base } from '@playwright/test';
-import { ContextPagesFixture, contextPagesFixture } from '../fixtures/context-pages';
-import { PlaywrightPagesFixture, playwrightPagesFixture } from '../fixtures/playwright-pages';
-import { combineFixtures } from '../utils/fixtures';
-
-export const searchTest = base.extend<ContextPagesFixture, PlaywrightPagesFixture>(
-  combineFixtures(contextPagesFixture, playwrightPagesFixture)
-);
