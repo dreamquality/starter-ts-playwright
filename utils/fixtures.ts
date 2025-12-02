bc42fc4f42e03f98d8d188022283d6d0a79a5612
dreamquality <dreamquality@users.noreply.github.com>
2025-12-02 08:55:57 +0000

    deploy: 51bf463102d0b8d8d2778a2a788ccd7a8cc404f5

--- utils/fixtures.ts
+++
@@ -1,4 +0,0 @@
-import { Fixtures } from '@playwright/test';
-
-export const combineFixtures = (...args: Fixtures[]): Fixtures =>
-  args.reduce((acc, fixture) => ({ ...acc, ...fixture }), {});
--- utils/mocks/static-mock.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from '@playwright/test';
-
-export const mockStaticRecourses = async (page: Page): Promise<void> => {
-  await page.route('**/*.{ico,png,jpg,mp3,woff,woff2}', (route) => route.abort());
-};
